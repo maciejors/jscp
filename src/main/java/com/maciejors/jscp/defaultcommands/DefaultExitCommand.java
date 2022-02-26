51f3d666a8b1fa42790f2d2ed23287f1711ee3e4
meagle <maciej.orslowski@gmail.com>
2022-02-26 16:53:02 +0100

    rename DefaultQuitCommand to DefaultExitCommand

--- src/main/java/com/maciejors/jscp/defaultcommands/DefaultExitCommand.java
+++ src/main/java/com/maciejors/jscp/defaultcommands/DefaultExitCommand.java
@@ -4,10 +4,10 @@ import com.maciejors.jscp.annotations.CommandDescription;
 import com.maciejors.jscp.core.Command;
 
 /**
- * Default quit command that can be registered by {@link com.maciejors.jscp.core.CommandManager}
+ * Default exit command that can be registered by {@link com.maciejors.jscp.core.CommandManager}
  */
-@CommandDescription("Quits the program using System.exit()")
-public class DefaultQuitCommand extends Command {
+@CommandDescription("Exits the program using System.exit()")
+public class DefaultExitCommand extends Command {
     @Override
     public String call(String[] args) {
         System.exit(0);
