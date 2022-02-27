7e2f1f3005c0ea302ded58f4ef08fc7f08b2f754
maciejors <maciej.orslowski@gmail.com>
2022-02-27 11:55:34 +0100

    remove null check on command in CommandCall.execute and create InvalidStatement class instead

--- src/main/java/com/maciejors/jscp/core/statements/CommandCall.java
+++ src/main/java/com/maciejors/jscp/core/statements/CommandCall.java
@@ -26,9 +26,6 @@ public class CommandCall extends Statement {
      */
     @Override
     public String execute() {
-        if (command == null) {
-            return "Error: command not found";
-        }
         return command.call(args);
     }
 }
---
+++ src/main/java/com/maciejors/jscp/core/statements/InvalidStatement.java
@@ -0,0 +1,15 @@
+package com.maciejors.jscp.core.statements;
+
+public class InvalidStatement extends Statement {
+
+    private final String errorMsg;
+
+    public InvalidStatement(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String execute() {
+        return errorMsg;
+    }
+}
